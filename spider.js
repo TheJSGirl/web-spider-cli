68880e9b750fab5ee41997043136a21ab3bf2c4b
thejsgirl <prinegi100@gmail.com>
2019-04-18 00:12:18 +0530

    refactor: minor change

--- spider.js
+++ spider.js
@@ -4,30 +4,31 @@ const path = require('path');
 const utilities = require('./utilities');
 const fs = require('fs');
 
-function spider(url, callback) {
+async function spider(url, callback) {
     const filename = utilities.urlToFilename(url);
     console.log(filename)
     fs.exists(filename, exists => {
         if(!exists) {
-            request(url, (err, response, body) => {
+            request(url, async(err, response, body) => {
                 if(err) {
+                    console.log("helloo")
                     callback(err)
                 }
                 else {
-                   const dirName = `${path.resolve(__dirname)}/${filename}`;
-                    mkdir.mkdirsSync(dirName, err => {
-                        if(err) {
-                            callback(err)
-                        }
-                        else{
-                            fs.writeFileSync(dirName, "body", err => {
+                   const dirName = `${path.resolve(__dirname)}/${filename}`;                    
+                   await  mkdir.mkdirsSync(dirName, err => {
+                        // if(err) {
+                        //     console.log("error")
+                        //     callback(err)
+                        // }
+                        fs.writeFileSync(`${path.resolve(__dirname)}/${filename}.html`, body, err => {
                                 if(err) {
                                     callback(err)
                                 }else {
                                     callback(null, filename, true);
                                 }
                             })
-                        }
+                        
                     })
                 }
             });
