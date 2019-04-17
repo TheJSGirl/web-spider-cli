f804201383094078c52fe2d5381563214ad39cde
TheJSGirl <prinegi100@gmail.com>
2019-04-18 01:52:11 +0530

    feat: added all the links inside downloads folder $ updated .gitignore

--- spider.js
+++ spider.js
@@ -1,48 +1,56 @@
-const request = require('request');
-const mkdir = require('mkdir');
-const path = require('path');
-const utilities = require('./utilities');
-const fs = require('fs');
+const request = require("request");
+const mkdir = require("mkdir");
+const path = require("path");
+const utilities = require("./utilities");
+const fs = require("fs");
 
 function spider(url, callback) {
-    const filename = utilities.urlToFilename(url);
-    fs.exists(filename, exists => {
-        if(!exists) {
-            request(url, (err, response, body) => {
-                if(err) {
-                    callback(err)
-                }
-                else {
-                   const dirName = path.resolve(__dirname, filename);
-                    mkdir.mkdirsSync(dirName, err => {
-                        if(err) {
-                            callback(err)
-                        }
-                    })
-
-                    fs.writeFileSync(`${dirName}/${filename}.html`, body, err => {
-                        if(err) {
-                            callback(err)
-                        }else {
-                            callback(null, filename, true);
-                        }
-                    })
-                }
-            });
+  const filename = utilities.urlToFilename(url);
+  fs.exists(filename, exists => {
+    if (!exists) {
+      request(url, (err, response, body) => {
+        if (err) {
+          callback(err);
         } else {
-            callback(null, filename, false);
+          const parentDirName = path.resolve(__dirname, "downloads");
+          mkdir.mkdirsSync(parentDirName, err => {
+            if (err) {
+              return;
+            }
+          });
+          mkdir.mkdirsSync(`${parentDirName}/${filename}`, err => {
+            if (err) {
+              callback(err);
+            }
+          });
+
+          fs.writeFileSync(
+            `${parentDirName}/${filename}/${filename}.html`,
+            body,
+            err => {
+              if (err) {
+                callback(err);
+              } else {
+                callback(null, filename, true);
+              }
+            }
+          );
         }
-    })
+      });
+    } else {
+      callback(null, filename, false);
+    }
+  });
 }
 
-function mycallback (err, filename, downloaded) {
-    if(err) {
+function mycallback(err, filename, downloaded) {
+  if (err) {
     console.log(err);
-    } else if(downloaded){
+  } else if (downloaded) {
     console.log(`Completed the download of "${filename}"`);
-    } else {
+  } else {
     console.log(`"${filename}" was already downloaded`);
-    }
-    }
-    
+  }
+}
+
 spider(process.argv[2], mycallback);
